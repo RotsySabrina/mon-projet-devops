2e430dd5cba37a0e43d0f92d94f93e9a139be9d0
RotsySabrina <fanovosoarotsy@gmail.com>
2025-12-22 19:45:58 +0300

    échec

--- src/calcul.js
+++ src/calcul.js
@@ -2,7 +2,7 @@ function addition(a, b) {
     if (typeof a !== 'number' || typeof b !== 'number') {
         throw new Error("Les arguments doivent être des nombres");
     }
-    return a + b;
+    return a - b;
 }
 
 module.exports = addition;
